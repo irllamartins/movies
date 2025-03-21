4a8448d6d1aa943ec712163473d30fc6c2af71a3
irllamartins <irllamartinsbarbosa@gmail.com>
2025-03-21 17:34:24 -0300

    feat:.env fuctional

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios'
 import { MovieDetails } from '../screens/Details'
- 
-const API_KEY="9bbbf4e53b66f49f1ea086ad10c8d949"
-const API_URL="https://api.themoviedb.org/3"
-const BEARER="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmJiZjRlNTNiNjZmNDlmMWVhMDg2YWQxMGM4ZDk0OSIsIm5iZiI6MTc0MjE1MTAyNC4zNiwic3ViIjoiNjdkNzFkNzBlYmMzMGEwYjQ4MDEzN2ZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.t4uGZcRqfqKd_PCrC5T76Xir6x3WlxzmmNTT0ZhcmbU"
+import { API_KEY, API_URL } from '@env'
 
 export const api = axios.create({
     baseURL: API_URL,
