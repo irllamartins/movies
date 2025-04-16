b156e5d59f4628aa5a26c9748a0b260fec2da25a
Irlla Martins <irllamartinsbarbosa@gmail.com>
2025-04-16 09:55:07 -0300

    fix: babel error

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets:  ['@react-native/babel-preset'],//['module:metro-react-native-babel-preset'],
     plugins: [
+     // ['@babel/plugin-transform-private-methods', { loose: true }],
       ["module:react-native-dotenv", {
         "envName": "APP_ENV",
         "moduleName": "@env",
